88a86875ef53a29cf0c53ee155b5fbd8edad8ee5
Bhavesh Odedra <odedra@emanju.de>
2018-04-06 13:42:44 +0530

    [ADD] generic module

---
+++ emanju/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2018 - TODAY, Emanju.de
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Emanju Odoo",
+    "summary": "provides the scope (dependencies), configuration and data of "
+               "Odoo",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Bhavesh Odedra, Emanju",
+    "category": "CRM",
+    "website": "emanju.de",
+    "depends": [
+        "mail",
+        "crm",
+        "hr",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
