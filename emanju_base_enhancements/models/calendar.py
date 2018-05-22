4d9743b193579f1b6692536a7e86cc316a72b755
Bhavesh Odedra <odedra@emanju.de>
2018-05-22 17:19:06 +0530

    [ADD] bubble notification when create calendar event based on Users calendar notification on/off

--- emanju_base_enhancements/models/calendar.py
+++ emanju_base_enhancements/models/calendar.py
@@ -18,56 +18,25 @@ class CalendarAttendee(models.Model):
         if self.template_id and self.template_id.body_html:
             self.description = html2text.html2text(self.template_id.body_html)
 
-    @api.multi
-    def create_attendees(self):
-        current_user = self.env.user
-        result = {}
-        for meeting in self:
-            alreay_meeting_partners = meeting.attendee_ids.mapped('partner_id')
-            meeting_attendees = self.env['calendar.attendee']
-            meeting_partners = self.env['res.partner']
-            for partner in meeting.partner_ids.filtered(lambda partner: partner not in alreay_meeting_partners):
-                user = self.env['res.users'].search(
-             [('partner_id', '=', partner.id)])
-                if user.calendar_notification_type == 'off':
-                    continue
-                values = {
-                    'partner_id': partner.id,
-                    'email': partner.email,
-                    'event_id': meeting.id,
-                }
-                # current user don't have to accept his own meeting
-                if partner == self.env.user.partner_id:
-                    values['state'] = 'accepted'
-
-                attendee = self.env['calendar.attendee'].create(values)
-
-                meeting_attendees |= attendee
-                meeting_partners |= partner
-
-            if meeting_attendees:
-                to_notify = meeting_attendees.filtered(lambda a: a.email != current_user.email)
-                to_notify._send_mail_to_attendees('calendar.calendar_template_meeting_invitation')
-
-                meeting.write({'attendee_ids': [(4, meeting_attendee.id) for meeting_attendee in meeting_attendees]})
-            if meeting_partners:
-                meeting.message_subscribe(partner_ids=meeting_partners.ids)
-
-            # We remove old attendees who are not in partner_ids now.
-            all_partners = meeting.partner_ids
-            all_partner_attendees = meeting.attendee_ids.mapped('partner_id')
-            old_attendees = meeting.attendee_ids
-            partners_to_remove = all_partner_attendees + meeting_partners - all_partners
-
-            attendees_to_remove = self.env["calendar.attendee"]
-            if partners_to_remove:
-                attendees_to_remove = self.env["calendar.attendee"].search([('partner_id', 'in', partners_to_remove.ids), ('event_id', '=', meeting.id)])
-                attendees_to_remove.unlink()
-
-            result[meeting.id] = {
-                'new_attendees': meeting_attendees,
-                'old_attendees': old_attendees,
-                'removed_attendees': attendees_to_remove,
-                'removed_partners': partners_to_remove
-            }
-        return result
+    @api.model
+    def create(self, vals):
+        res = super(CalendarAttendee, self).create(vals)
+        my_list = [x.id for x in res.partner_ids]
+        user_list = self.env['res.users'].search(
+                    [('partner_id', 'in', my_list),
+                     ('calendar_notification_type', '=', 'on')])
+        new_partner_list = [x.partner_id.id for x in user_list]
+        message = self.env['mail.message'].create(
+            {'body': res.description,
+             'subject': res.name,
+             'message_type': 'comment',
+             'subtype_id': self.env.ref('mail.mt_comment').id,
+             'res_id': res.id,
+             'author_id': 1,
+             'model': 'calendar.event',
+             'partner_ids' : [(6, 0, new_partner_list)]})
+        for partner in new_partner_list:
+            noti = self.env['mail.notification'].create(
+                {'mail_message_id' : message.id,
+                 'res_partner_id' : partner})
+        return res
